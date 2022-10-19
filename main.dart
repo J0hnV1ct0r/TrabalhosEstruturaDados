a800cfc234a34c612410fe5cafa1c63115c4661b
João Victor de S. Albuquerque <84153487+J0hnV1ct0r@users.noreply.github.com>
2022-10-19 19:01:39 +0000

    ajeitando a logica

--- main.dart
+++ main.dart
@@ -20,9 +20,12 @@ class Lista {
   //funçao PushOperador:
   void pushOperador(var add) {
     for(int y = 0; y < add.lista.length; y++){
-      this.exp = this.exp + " ${add.lista[y]}";
+      if(add.lista[y] == "("){
+        this.exp = this.exp + " ${add.pop()}"; 
+      }else{
+       add.pop();
+      }
     }
-    add.lista = [];
   }
   //função Pop:
   pop() {
@@ -63,7 +66,9 @@ convercao(var pilhaNum, var expressao){
          expressao[t] == "/") {
        espera.push(expressao[t]);
      } else {
-       if (expressao[t] != "(") {
+       if (expressao[t] == "(") {
+         espera.push(expressao[t]);
+       }else{
          pilhaNum.push(expressao[t]);
        }
      }
